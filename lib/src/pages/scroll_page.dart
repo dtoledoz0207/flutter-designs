cb84fb209ead5cc367b5d7fbec0f0639365918a8
David Toledo <dtoledoz0207@gmail.com>
2020-09-09 13:03:45 -0500

    Scroll Design - part-2

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -16,10 +16,52 @@ class ScrollPage extends StatelessWidget {
   }
 
   Widget _page1() {
-    return Center(
-      child: Text('page 1'),
+    return Stack(
+      children: <Widget>[
+        _backgroundColor(),
+        _backgroundImage(),
+        _infoTexts()
+      ],
     );
   }
+
+  Widget _backgroundColor(){
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+    );
+  }
+
+  Widget _backgroundImage() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage('assets/scroll-1.png'),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _infoTexts() {
+    final textStyle = TextStyle(color: Colors.white, fontSize: 50.0);
+
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 20.0),
+          Text('11°', style: textStyle,),
+          Text('Miercoles', style: textStyle,),
+          Expanded(child: Container()),
+          Icon(Icons.keyboard_arrow_down, size: 70.0, color: Colors.white,)
+        ],
+      ),
+    );
+  }
+
+
+
 
   Widget _page2() {
     return Center(
